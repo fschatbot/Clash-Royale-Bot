31c0b2203b6bfd68fe7e1ea41209521c3def2d13
Parv-Jain369 <parvjain369@gmail.com>
2025-09-04 19:31:32 +0530

    Proper Screenshot Protocol

--- index.py
+++ index.py
@@ -1,17 +1,31 @@
 from PIL import ImageGrab
+import time
+import win32gui
+import win32con
+import pyautogui
 
+screen_width, screen_height = pyautogui.size()
+game_width = 1262 - 656
+bbox = (screen_width / 2 - game_width / 2, 0, screen_width / 2 + game_width / 2, screen_height)  # Full screen
+
+def bring_focus():
+    window_title = "BlueStacks App Player"
+    hwnd = win32gui.FindWindow(None, window_title)
+    if hwnd == 0: raise Exception("Window not found: " + window_title)
+
+    win32gui.ShowWindow(hwnd, win32con.SW_RESTORE)
+    win32gui.SetForegroundWindow(hwnd)
+
+    while True:
+        if win32gui.GetForegroundWindow() == hwnd: break
+        time.sleep(0.1)
+
+    return hwnd
 
 def screenshot():
-    bbox = (690, 145, 1179, 808)  # right = left + width, bottom = top + height
-
-# Capture the region
+    bring_focus()
     screenshot= ImageGrab.grab(bbox=bbox)
-    grayscale_screenshot= screenshot.convert("L")
-
-# Save the file
-    grayscale_screenshot.save("region_screenshot_pil.png")
-
-    print("Region screenshot saved as region_screenshot_pil.png")
+    return screenshot
 
 
 def is_end_screen():
@@ -25,4 +39,5 @@ def main():
     ...
 
 if __name__ == "__main__":
-    screenshot()
+    ss = screenshot()
+    ss.show()
