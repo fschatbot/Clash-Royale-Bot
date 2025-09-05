80df71c5b034d7f94a52827cfd803047d3b66aca
Parv-Jain369 <parvjain369@gmail.com>
2025-09-05 19:07:35 +0530

    C

--- index.py
+++ index.py
@@ -1,12 +1,16 @@
-from PIL import ImageGrab
+from PIL import ImageGrab, Image, ImageChops
 import time
 import win32gui
 import win32con
 import pyautogui
+import numpy as np 
+import os
 
 screen_width, screen_height = pyautogui.size()
 game_width = 1262 - 656
 bbox = (screen_width / 2 - game_width / 2, 0, screen_width / 2 + game_width / 2, screen_height)  # Full screen
+
+current_i = len(os.listdir('database'))
 
 def bring_focus():
     window_title = "BlueStacks App Player"
@@ -23,9 +27,30 @@ def bring_focus():
     return hwnd
 
 def screenshot():
+    global current_i
+
     bring_focus()
-    screenshot= ImageGrab.grab(bbox=bbox)
-    return screenshot
+    screenshot = ImageGrab.grab(bbox=bbox)
+    screenshot = screenshot.convert('L')
+    # Save this in the database folder
+    screenshot.save(f'database/test{current_i}.png')
+    current_i += 1
+
+def page(ss: Image.Image):
+    # Find if start.png is present in ss at (230, 810)
+    start_menu = Image.open('assets/start.png').convert('L')
+    region = ss.crop((230, 810, 230 + start_menu.width, 810 + start_menu.height))
+    difference = ImageChops.difference(region, start_menu)
+    if np.max(np.array(difference)) == 5:
+        return 'START_PAGE'
+    
+    
+
+    return np.max(np.array(difference))
+    
+
+    raise Exception("Unknown page")
+
 
 
 def is_end_screen():
@@ -40,4 +65,6 @@ def main():
 
 if __name__ == "__main__":
     ss = screenshot()
-    ss.show()
+    ss.save("screenshot.png")
+    # ss.show()
+    print(page(ss))
